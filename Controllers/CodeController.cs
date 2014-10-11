f4b5de84f7faca8016ef468315a7926a2aed7208
unknown <Sumit Jolly@GGN000056637007.ads.mckinsey.com>
2014-10-11 21:03:52 +0530

    first commit

---
+++ Controllers/CodeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LetsWriteCode.Controllers
+{
+    public class CodeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.message = "Welcome to Healthy Code magazine - November issue!";
+            return View();
+        }
+    }
+}
